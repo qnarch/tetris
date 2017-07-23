47ab574a14915acb26097e5f8df02f24f1b66b17
Alexander Hjelm <admin@alexander-hjelm.com>
2017-07-23 15:20:02 +0200

    Player input management

--- main.lua
+++ main.lua
@@ -14,3 +14,13 @@ function love.update(dt)
   playgrid:update(dt)
 end
 
+function love.keypressed(key)
+    if key=="a" or key=="left" then
+        playgrid:updateActivePos(-1, 0)
+    elseif key=="d" or key=="right" then
+        playgrid:updateActivePos(1, 0)
+    end
+    if key=="w" or key=="up" then
+        playgrid:rotateActiveShape(1)
+    end
+end
